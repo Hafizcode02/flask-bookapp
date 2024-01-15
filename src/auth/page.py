0938165e9f3c67a585127049ca1fa044e7230f57
Hafiz Caniago <hafizcode02@gmail.com>
2024-01-15 21:05:48 +0700

    feat: test method post & request form

--- src/auth/page.py
+++ src/auth/page.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, redirect, url_for, session, flash
 from jinja2 import TemplateNotFound
 
 authPage = Blueprint('authPage', __name__,
@@ -8,6 +8,18 @@ authPage = Blueprint('authPage', __name__,
 def login():
     return render_template('login.html')
     
-@authPage.route("/register")
+@authPage.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'POST':
+        if all(request.form.get(key) is not None and request.form.get(key) != '' for key in ['name', 'email', 'password', 'password_confirmation']):
+            if request.form.get('password') == request.form.get('password_confirmation'):
+                return "Lanjut"
+            else:
+                flash("Password and password confirmation must be same")
+                return redirect(url_for('authPage.register'))
+        else:
+            flash("All form must be filled")
+            return redirect(url_for('authPage.register'))
+        
+    elif request.method == 'GET':
+        return render_template('register.html')
