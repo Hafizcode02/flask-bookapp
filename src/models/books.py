dd6b36ca38de4ba4ab519d500fa7e7f0216c827d
Hafiz Caniago <hafizcode02@gmail.com>
2024-01-16 15:33:30 +0700

    feat: define __init__ model

--- src/models/books.py
+++ src/models/books.py
@@ -1,8 +1,15 @@
 from database import db_instance as db
 
-class Book(db.Model):
+class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     judul = db.Column(db.String(255), nullable=False)
     penulis = db.Column(db.String(255), nullable=False)
     penerbit = db.Column(db.String(255), nullable=False)
     tahun_terbit = db.Column(db.String(10), nullable=False)
+    
+    def __init__(self, judul, penulis, penerbit, tahun_terbit):
+        self.judul = judul
+        self.penulis = penulis
+        self.penerbit = penerbit
+        self.tahun_terbit = tahun_terbit
+        
