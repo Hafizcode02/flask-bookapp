fbba7bc3c439dc3008951ee8a79a684410e4ce7e
Hafiz Caniago <hafizcode02@gmail.com>
2024-01-16 15:33:00 +0700

    feat: add check session before access

--- src/dashboard/page.py
+++ src/dashboard/page.py
@@ -4,9 +4,11 @@ from jinja2 import TemplateNotFound
 dashboardPage = Blueprint('dashboardPage', __name__,
                         template_folder='templates')
 
+@dashboardPage.before_request
+def check_session():
+    if 'loggedIn' not in session or session['loggedIn'][0] == False:
+        return redirect(url_for('authPage.login'))
+
 @dashboardPage.route("/dashboard")
 def dashboard():
-    if 'loggedIn' in session and session['loggedIn'][0] == True:
-        return render_template('dashboard.html', name=session['userName'][0])
-    else:
-        return redirect(url_for('authPage.login'))
+    return render_template('dashboard.html', name=session['userName'][0])
